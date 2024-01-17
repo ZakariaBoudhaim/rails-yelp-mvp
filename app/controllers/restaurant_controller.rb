37e95a93f0cebc6fce2a23db90c6c18e12430e18
Zakariaboudhaim <zakariaboudhaim@gmail.com>
2024-01-17 17:28:08 +0100

    restaurant application using rails 85% done

---
+++ app/controllers/restaurant_controller.rb
@@ -0,0 +1,2 @@
+class RestaurantController < ApplicationControllerend
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# db/seeds.rb
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: +332_154_154, category: "italian" }
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St", phone_number: +332_154_154, category: "japanese" }
+lewagon = { name: "lewagon", address: "casablanca", phone_number: +332_548_184, category: "french" }
+
+[dishoom, pizza_east, lewagon].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
